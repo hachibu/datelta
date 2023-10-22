6f8421920d81a48f8729bb7bebb15c291bbc0fdd
Raymond Sohn <raymondsohn@gmail.com>
2023-10-22 11:16:57 -0700

    Improve CLI argument handling

--- src/main.ts
+++ src/main.ts
@@ -7,16 +7,15 @@ const help = `Usage: datelta ${version}
 Datelta calculates the difference between 2 dates.
 
 Options:
-          --start     Start date
-          --end       End date
-      -h, --help      Print help
-      -V, --version   Print version`;
+          --start     Start date (default: now).
+          --end       End date (default: now).
+      -h, --help      Print help.
+      -V, --version   Print version.`;
 
 function main() {
   const flags = parse(Deno.args, {
     boolean: ["help", "h", "version", "V"],
-    string: ["start", "s", "end", "e"],
-    default: { color: true },
+    string: ["start", "end"],
   });
 
   if (flags.help || flags.h) {
@@ -25,24 +24,20 @@ function main() {
     return console.log(version);
   }
 
-  let startDate, endDate;
-
-  if (flags.start) {
-    startDate = new Date(flags.start);
-    endDate = new Date();
-  }
+  let startDate = new Date();
+  let endDate = new Date();
 
   if (flags.start && flags.end) {
     startDate = new Date(flags.start);
     endDate = new Date(flags.end);
+  } else if (flags.start) {
+    startDate = new Date(flags.start);
   }
 
-  if (!startDate) {
-    console.error("Error: Start date is required.");
-  } else if (startDate && endDate) {
-    const diff = dateDiff(startDate, endDate);
-    console.log(JSON.stringify(diff));
-  }
+  const diff = dateDiff(startDate, endDate);
+  const diffJSON = JSON.stringify(diff);
+
+  console.log(diffJSON);
 }
 
 if (import.meta.main) {
